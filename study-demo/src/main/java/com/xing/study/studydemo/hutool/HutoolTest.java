b25974926928f2247ca2042ee30181d415b84f0c
guyexin <guyexin@126.com>
2020-09-18 17:27:46 +0800

    hutool增加身份证方法

--- study-demo/src/main/java/com/xing/study/studydemo/hutool/HutoolTest.java
+++ study-demo/src/main/java/com/xing/study/studydemo/hutool/HutoolTest.java
@@ -12,6 +12,7 @@ package com.xing.study.studydemo.hutool;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.lang.Console;
 import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.IdcardUtil;
 import cn.hutool.core.util.RandomUtil;
 
 import java.util.Date;
@@ -26,10 +27,21 @@ public class HutoolTest {
 
     public static void main(String[] args) {
         Console.log("1111");
+        // 唯一ID工具-IdUtil
         Console.log(IdUtil.fastSimpleUUID());
         Console.log(IdUtil.getSnowflake(1L,1L).nextId());
+
+        // 随机数工具
         Console.log(RandomUtil.randomLong());
+
+        // 日期工具
         Console.log(DateUtil.formatDateTime(new Date()));
+
+        // 身份证工具
+        Console.log(IdcardUtil.isValidCard("441502198810055038"));
+        Console.log(IdcardUtil.getProvinceByIdCard("441502198810055038"));
+        Console.log(IdcardUtil.getAgeByIdCard("441502198810055038"));
+        Console.log(IdcardUtil.getGenderByIdCard("370829195708293252"));
 
     }
 }
